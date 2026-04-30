5ffe3b46f5831effcbbf7146fb24ff88bd018851
Basmala Wael <basmalawael86@gmail.com>
2026-04-30 23:52:42 +0700

    EventConfiguration

--- G-NET-12-EF03/Model/EventConfiguration.cs
+++ G-NET-12-EF03/Model/EventConfiguration.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace G_NET_12_EF02.Model
 {
     internal class EventConfiguration : IEntityTypeConfiguration<Event>
-    {    
+    {     
         public void Configure(EntityTypeBuilder<Event> builder)
         {
          builder.HasKey(E => E.EventID);
